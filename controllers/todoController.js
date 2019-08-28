bcd2c6c3ecfe4582add2bd238ee000503378cdf0
Miguel Lopes <miguelpvz@hotmail.com>
2019-08-28 13:14:01 +0100

    Add support for MongoDB

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,11 +1,20 @@
+var bodyParser = require('body-parser');
+
+var data = [{item: "Get Milk"},{item: "Get rich"},{item: "Kick some ass"}]
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
 module.exports = function(app) {
     app.get('/todo', (req, res) => {
-        res.render('todo');
+        res.render('todo',{todos: data});
     })
-    app.post('/todo', (req, res) => {
-        
+    app.post('/todo', urlencodedParser, (req, res) => {
+        data.push(req.body);
+        res.json(data);
     })
-    app.delete('/todo', (req, res) => {
-        
+    app.delete('/todo/:item', (req, res) => {
+        data = data.filter((todo) => {
+            return todo.item.replace(/ /g, '-') !== req.params.item;
+        });
+        res.json(data);
     })
 }
