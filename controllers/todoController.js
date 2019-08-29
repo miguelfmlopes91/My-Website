4d3faabb57312716ef23b42e86c8b7ef8111563e
Miguel Lopes <miguel.lopes@miniclip.com>
2019-08-29 07:56:24 +0100

    wip DB

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,4 +1,25 @@
 var bodyParser = require('body-parser');
+const MongoClient = require('mongoose');
+
+//Prepare url for connection
+const uri = "mongodb+srv://test:test@todo-itvro.mongodb.net/test?retryWrites=true&w=majority";
+
+//connect to the database
+MongoClient.connect(uri, { useNewUrlParser: true });
+
+//Create a schema
+var todoSchema = new MongoClient.Schema({
+    item: String
+});
+//The Model type
+var Todo = MongoClient.model('Todo', todoSchema);
+
+//test
+var item1 =  Todo({item: 'Buy flowers'}).save((error) => {
+    if(error) throw error;
+    console.log("Item saved");
+    
+});
 
 var data = [{item: "Get Milk"},{item: "Get rich"},{item: "Kick some ass"}]
 var urlencodedParser = bodyParser.urlencoded({extended: false});
